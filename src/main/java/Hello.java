4f41c2f1d734a3a7bddcdb4684063dba63225c0e
Jason <bboylilshaw@gmail.com>
2015-07-13 22:05:28 +0800

    Initial commit

---
+++ src/main/java/Hello.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Jason on 7/13/15.
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
---
+++ src/test/java/HelloTest.java
@@ -0,0 +1,8 @@
+import static org.junit.Assert.*;
+
+/**
+ * Created by Jason on 7/13/15.
+ */
+public class HelloTest {
+
+}

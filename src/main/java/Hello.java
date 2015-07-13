378d9e11834a3ee3712df7aff4a39f620bbf3b42
Jason <bboylilshaw@gmail.com>
2015-07-13 22:12:59 +0800

    refactor Hello

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -5,4 +5,9 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("hello");
     }
+
+    public int add(int x, int y) {
+        return x + y;
+    }
+
 }
